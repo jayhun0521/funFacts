0007003607023f3089e016b618a70947f76569bc
jayhun0521 <jayhun21@gmail.com>
2022-11-17 15:50:48 -0500

    updating css and js files

--- src/components/PostThree.js
+++ src/components/PostThree.js
@@ -3,7 +3,7 @@ import React from 'react'
 const PostOne = () => {
     return (
       <div className='container'>
-       <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/28/Bradley_Advanced_Running_Gear.jpg/1280px-Bradley_Advanced_Running_Gear.jpg" className='photo' />
+       <img src="https://www.thedefensepost.com/wp-content/uploads/2022/02/bradley-3413336-1170x610.jpg" className='photo' />
         <h2 className='center'>M2 Bradley Linebacker Fighting Vehicle</h2>
         <p>The M2 Bradley Infantry Fighting Vehicle (IFV) consists of five variants: the M2, M2A1, M2A2, M2A2 ODS (Operation Desert Storm improvements), and M2A3. Their main mission is to provide protected transport of an infantry squad (up to six passengers) to critical points. Aside from carrying mechanized infantry into close contact with the enemy, the M2 can also provide overwatching fire to dismounting infantrymen. It is adequately armored to provide protection against small arms fire and artillery, and able to combat any vehicle on the battlefield using its TOW missiles. The M2 IFV also has six external firing ports for the squad M231 Firing Port Weapon on the M2 and M2A1 versions only. Four ports were removed on the sides of the vehicle on the M2A2–A3 versions, and only two in the ramp remain. These ports allow passengers to engage the enemy from within the protection of the Bradley vehicle. These firing ports are almost always covered by additional armor kits and a Bradley with them operable is rare. The proper use of M231 FPWs was rare in practice. The M2 Bradley unit cost is $1.11 million (FY 1993 constant dollars)US$1.84M in 2016 (inflation adjusted).</p>
       </div>
